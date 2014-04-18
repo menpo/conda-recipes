26a3991b84903fe5a02d0cde73754ba720eea7d9
patricksnape <patricksnape@gmail.com>
2014-04-18 14:10:06 +0100

    Handle 64-bit windows

--- assimp/bld.bat
+++ assimp/bld.bat
@@ -2,12 +2,23 @@
 
 mkdir build
 cd build
-cmake .. ^
+
+rem Need to handle Python 3.x case at some point (Visual Studio 2010)
+if %ARCH%==32 AND %PY_VER%<3 (
+  set CMAKE_GENERATOR="Visual Studio 9 2008"
+  set CMAKE_CONFIG="Release|Win32"
+)
+if %ARCH%==64 AND %PY_VER%<3 (
+  set CMAKE_GENERATOR="Visual Studio 9 2008 Win64"
+  set CMAKE_CONFIG="Release|x64"
+)
+
+cmake .. -G%CMAKE_GENERATOR% ^
  -DENABLE_BOOST_WORKAROUND=1 ^
  -DBUILD_ASSIMP_TOOLS=0 ^
  -DINCLUDE_INSTALL_DIR=%LIBRARY_INC% ^
  -DLIB_INSTALL_DIR=%LIBRARY_LIB% ^
  -DBIN_INSTALL_DIR=%LIBRARY_BIN%
 
-cmake --build . --config "Release|Win"%ARCH% --target ALL_BUILD
-cmake --build . --config "Release|Win"%ARCH% --target INSTALL
+cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
+cmake --build . --config %CMAKE_CONFIG% --target INSTALL
