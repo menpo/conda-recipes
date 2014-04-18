b988894ed0501bb2358185d49bc7a5221bb988e9
patricksnape <patricksnape@gmail.com>
2014-04-18 14:22:09 +0100

    Add 64-bit GLEW

--- glew/bld.bat
+++ glew/bld.bat
@@ -1,8 +1,21 @@
 @echo off
 
-mkdir output
-cd output
-cmake .. -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+mkdir build
+cd build
 
-cmake --build . --config "Release|Win"%ARCH% --target ALL_BUILD
-cmake --build . --config "Release|Win"%ARCH% --target INSTALL
+rem Need to handle Python 3.x case at some point (Visual Studio 2010)
+if %ARCH%==32 AND %PY_VER%<3 (
+  set CMAKE_GENERATOR="Visual Studio 9 2008"
+  set CMAKE_CONFIG="Release|Win32"
+)
+if %ARCH%==64 AND %PY_VER%<3 (
+  set CMAKE_GENERATOR="Visual Studio 9 2008 Win64"
+  set CMAKE_CONFIG="Release|x64"
+)
+
+cmake .. -G%CMAKE_GENERATOR% ^
+ -DBUILD_SHARED_LIBS=1 ^
+ -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+
+cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
+cmake --build . --config %CMAKE_CONFIG% --target INSTALL
