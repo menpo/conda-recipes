6e32cdc20acee1c64758d56e382d089b027cf8c6
Patrick Snape <patricksnape@gmail.com>
2014-04-19 03:38:48 -0700

    Incorrect boolean logic for batch files
    Was falling through and always building 64-bit versions

--- glew/bld.bat
+++ glew/bld.bat
@@ -4,13 +4,17 @@ mkdir build
 cd build
 
 rem Need to handle Python 3.x case at some point (Visual Studio 2010)
-if %ARCH%==32 AND %PY_VER%<3 (
-  set CMAKE_GENERATOR="Visual Studio 9 2008"
-  set CMAKE_CONFIG="Release|Win32"
+if %ARCH%==32 (
+  if %PY_VER% LSS 3 (
+    set CMAKE_GENERATOR="Visual Studio 9 2008"
+    set CMAKE_CONFIG="Release|Win32"
+  )
 )
-if %ARCH%==64 AND %PY_VER%<3 (
-  set CMAKE_GENERATOR="Visual Studio 9 2008 Win64"
-  set CMAKE_CONFIG="Release|x64"
+if %ARCH%==64 (
+  if %PY_VER% LSS 3 (
+    set CMAKE_GENERATOR="Visual Studio 9 2008 Win64"
+    set CMAKE_CONFIG="Release|x64"
+  )
 )
 
 cmake .. -G%CMAKE_GENERATOR% ^
