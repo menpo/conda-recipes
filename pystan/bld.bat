35f469a1951a193a0066660a5eda61ef5202eba3
Patrick Snape <patricksnape@gmail.com>
2014-04-29 16:22:28 +0100

    A pystan recipe!

---
+++ pystan/bld.bat
@@ -0,0 +1,11 @@
+echo "Copying stdint.h for windows"
+cp %LIBRARY_INC%\stdint.h %SRC_DIR%\pystan\stan\src\stdint.h > nul
+
+"%PYTHON%" setup.py install --single-version-externally-managed --record=%TEMP%record.txt
+if errorlevel 1 exit 1
+
+:: Add more build steps here, if they are necessary.
+
+:: See
+:: https://github.com/ContinuumIO/conda/blob/master/conda/builder/README.txt
+:: for a list of environment variables that are set during the build process.
