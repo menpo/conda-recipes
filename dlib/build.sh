fc0e4d1e1b5abd11860f4a2d845662642d8f2740
Patrick Snape <patricksnape@gmail.com>
2014-08-31 17:48:07 +0100

    Update dlib package to latest version

--- dlib/build.sh
+++ dlib/build.sh
@@ -7,7 +7,7 @@ CMAKE_ARCH="-m"$ARCH
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   export CFLAGS="$CFLAGS -fPIC $CMAKE_ARCH"
-  export LDLAGS="$LDLAGS $CMAKE_ARCH"
+  export LDFLAGS="$LDFLAGS $CMAKE_ARCH"
   DYNAMIC_EXT="so"
 fi
 if [ "$(uname -s)" == "Darwin" ]; then
@@ -17,15 +17,17 @@ fi
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 
 cmake ../tools/python -G"$CMAKE_GENERATOR" \
+-DCMAKE_PREFIX_PATH=$PREFIX \
 -DBUILD_SHARED_LIBS=1 \
 -DBoost_USE_STATIC_LIBS=0 \
 -DBoost_USE_STATIC_RUNTIME=0 \
 -DBOOST_INCLUDEDIR=$PREFIX/include \
 -DBOOST_LIBRARYDIR=$PREFIX/lib \
 -DPYTHON_LIBRARY=$PREFIX/lib/libpython$PY_VER.$DYNAMIC_EXT \
--DPYTHON_INCLUDE=$PREFIX/lib/python$PY_VER
+-DPYTHON_INCLUDE_DIR=$PREFIX/include/python$PY_VER \
+-DDLIB_NO_GUI_SUPPORT=1
 
 cmake --build . --config Release --target install
 
-cp dlib.$DYNAMIC_EXT $SP_DIR
+cp dlib.so $SP_DIR
 
