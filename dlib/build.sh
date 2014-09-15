1d295ad11249780b8e6766920017668d6a860511
Patrick Snape <patricksnape@gmail.com>
2014-09-15 12:08:42 +0100

    Remove blas support by default (before features fix)

--- dlib/build.sh
+++ dlib/build.sh
@@ -25,7 +25,10 @@ cmake ../tools/python -G"$CMAKE_GENERATOR" \
 -DBOOST_LIBRARYDIR=$PREFIX/lib \
 -DPYTHON_LIBRARY=$PREFIX/lib/libpython$PY_VER.$DYNAMIC_EXT \
 -DPYTHON_INCLUDE_DIR=$PREFIX/include/python$PY_VER \
--DDLIB_NO_GUI_SUPPORT=1
+-DDLIB_NO_GUI_SUPPORT=1 \
+-DDLIB_USE_BLAS=0 \
+-DDLIB_USE_LAPACK=0
+# Above should be changed when the MKL features problem is solved
 
 cmake --build . --config Release --target install
 
