6f9b52e7b27c60ba98be6b3caa148a1747dfbd64
Patrick Snape <patricksnape@gmail.com>
2015-02-06 09:14:18 +0000

    Remove unused recipes

--- menpo/build.sh
+++
@@ -1,8 +0,0 @@
-MAKE_ARCH="-m"$ARCH
-
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  export CFLAGS="$CFLAGS $MAKE_ARCH"
-  export LDLAGS="$LDLAGS $MAKE_ARCH"
-fi
-
-"$PYTHON" setup.py install --single-version-externally-managed --record=/tmp/record.txt
