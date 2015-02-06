8adb80720fb07948da5b214a3d2a9b122585b4a8
Patrick Snape <patricksnape@gmail.com>
2015-02-06 10:20:26 +0000

    Update scikits sparse
    Trying to sort out mystery segfault when running nricp code

--- scikits-sparse/build.sh
+++ scikits-sparse/build.sh
@@ -1,2 +1,4 @@
-"$PYTHON" setup.py install --single-version-externally-managed --record=/tmp/record.txt
+export CFLAGS="-I$INCLUDE_PATH"
+export LDFLAGS="-L$LIBRARY_PATH"
 
+"$PYTHON" setup.py install --single-version-externally-managed --record=/tmp/record.txt
