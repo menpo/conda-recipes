690f73cda29b599adc5a2b46b6c340c088638533
Patrick Snape <patricksnape@gmail.com>
2015-02-23 20:40:08 +0000

    Add avx and non-avx build of vlfeat

--- vlfeat_avx/build.sh
+++ vlfeat_avx/build.sh
@@ -16,8 +16,7 @@ fi
 export CFLAGS="-I$INCLUDE_PATH"
 export LDFLAGS="-L$LIBRARY_PATH"
 
-# Turn off all optimisations. Use vlfeat_avx for a fast version
-make NO_TESTS=yes ARCH=${VL_ARCH} DISABLE_SSE2=yes DISABLE_AVX=yes DISABLE_THREADS=yes DISABLE_OPENMP=yes-j${CPU_COUNT}
+make ARCH=${VL_ARCH} -j${CPU_COUNT}
 
 mkdir -p $PREFIX/bin
 cp bin/${VL_ARCH}/sift $PREFIX/bin/
