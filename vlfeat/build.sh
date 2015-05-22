af8db40cc412adecadfe3c32a23ceffedd8cefdd
Patrick Snape <patricksnape@gmail.com>
2015-05-22 09:27:05 +0100

    Fix for vlfeat on OSX
    Apparently the dylib doesn't link properly, so copy it into bin.
    This isn't really a problem for the dylib itself, just for the
    couple of binaries provided by vlfeat.

--- vlfeat/build.sh
+++ vlfeat/build.sh
@@ -19,6 +19,15 @@ export LDFLAGS="-L$LIBRARY_PATH"
 # Turn off all optimisations. Use vlfeat_avx for a fast version
 make NO_TESTS=yes ARCH=${VL_ARCH} DISABLE_SSE2=yes DISABLE_AVX=yes DISABLE_THREADS=yes DISABLE_OPENMP=yes -j${CPU_COUNT}
 
+# On OSX the resolution of the libvl.dylib doesn't seem to
+# work properly for the executables - but they are properly
+# relatively linked using @loader_path, therefore, copy
+# the dylib into bin so they work properly.
+if [ "$(uname -s)" == "Darwin" ]; then
+  mkdir -p $PREFIX/bin/
+  cp bin/${VL_ARCH}/libvl.${DYNAMIC_EXT} $PREFIX/bin/
+fi
+
 mkdir -p $PREFIX/bin
 cp bin/${VL_ARCH}/sift $PREFIX/bin/
 cp bin/${VL_ARCH}/mser $PREFIX/bin/
@@ -29,3 +38,4 @@ cp bin/${VL_ARCH}/libvl.${DYNAMIC_EXT} $LIBRARY_PATH/
 
 mkdir -p $INCLUDE_PATH/vl
 cp vl/*.h $INCLUDE_PATH/vl
+
