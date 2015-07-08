c06dc038149fb53a6a4741c00bc6a0a21c11266a
Patrick Snape <patricksnape@gmail.com>
2015-07-08 10:40:42 +0100

    Trying to get pymatbridge building on Windows
    Having linker errors at the moment...

--- pymatbridge/bld.bat
+++ pymatbridge/bld.bat
@@ -1,2 +1,35 @@
+if %ARCH%==32 (
+    set MLAB_ARCH="mexw32"
+)
+if %ARCH%==64 (
+    set MLAB_ARCH="mexw64"
+)
+
+rem Build the messenger for each platform to make sure
+rem everything is linked correctly. At the moment
+rem this only builds Matlab because I do not have Octave installed.
+cd pymatbridge\messenger
+rem Make the directory if it doesn't exist (e.g. Windows 32-bit)
+if NOT EXIST %MLAB_ARCH% (
+    md %MLAB_ARCH%
+)
+
+rem Create the config file for ZMQ - let make.py work out Matlab path
+"%PYTHON%" "%RECIPE_DIR%\write_config.py"
+mv local.cfg "%MLAB_ARCH%\local.cfg"
+
+rem Currently bug trying to build as c - move to cpp
+rem This has already been patched in make.py in the meta.yaml
+mv src\messenger.c src\messenger.cpp
+
+rem Build statically on Windows
+"%PYTHON%" make.py matlab
+#"%PYTHON%" make.py octave
+
+rem Quit early if making messenger failed
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cd ../..
 "%PYTHON%" setup.py install
-if errorlevel 1 exit 1
+
+if %errorlevel% neq 0 exit /b %errorlevel%
--- pymatbridge/write_config.py
+++ pymatbridge/write_config.py
@@ -1,17 +1,24 @@
 from os.path import join
 from os import environ
+import platform
+
+is_windows = platform.system().lower() == 'windows'
 
 if __name__ == "__main__":
-    PREFIX_PATH = environ.get('PREFIX')
-    INCLUDE_PATH = join(PREFIX_PATH, 'include')
-    LIBRARY_PATH = join(PREFIX_PATH, 'lib')
+    if not is_windows:
+        PREFIX_PATH = environ.get('PREFIX')
+        INCLUDE_PATH = join(PREFIX_PATH, 'include')
+        LIBRARY_PATH = join(PREFIX_PATH, 'lib')
+    else:
+        INCLUDE_PATH = environ.get('LIBRARY_INC')
+        LIBRARY_PATH = environ.get('LIBRARY_LIB')
     
     # Let the make script guess the directory of Matlab and Octave
     config = r"""MATLAB_BIN=
 OCTAVE_INC=
 OCTAVE_LIB=
-ZMQ_INC={}
-ZMQ_LIB={}
+ZMQ_INC="{}"
+ZMQ_LIB="{}"
 """.format(INCLUDE_PATH, LIBRARY_PATH)
     
     with open('local.cfg', 'w') as f:
