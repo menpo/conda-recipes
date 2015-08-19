291fa939c25327a710dbe6467d4534e29ce908be
Patrick Snape <patricksnape@gmail.com>
2015-08-19 21:13:39 +0100

    Add CMAKE build for OSX

--- cmake/build.sh
+++ cmake/build.sh
@@ -1,3 +1,14 @@
-chmod 755 cmake_install.sh
-./cmake_install.sh --prefix=$PREFIX --skip-license
+if [ "$(uname -s)" == "Darwin" ]; then
+  ./bootstrap               \
+    --prefix=$PREFIX        \
+    --no-qt-gui             \
+    --parallel=${CPU_COUNT} \
+    --no-system-libs
+
+  make -j${CPU_COUNT}
+  make install
+else
+  chmod 755 cmake_install.sh
+  ./cmake_install.sh --prefix=$PREFIX --skip-license
+fi
 
