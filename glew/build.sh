fd1571a0b56f7f8aa9e7a5bac3d68ecdd4185d54
Patrick Snape <patricksnape@gmail.com>
2016-05-11 13:10:03 +0200

    Trying osmesa glew

--- glew/build.sh
+++ glew/build.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-mkdir build
+
+CFLAGS="-I/Users/pts08/gits/mesa/test/include"
+LDFLAGS="-L/Users/pts08/gits/mesa/test/lib"
+
 cd build
 
-CMAKE_GENERATOR="Unix Makefiles"
-CMAKE_ARCH="-m"$ARCH
-
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  export CFLAGS="$CFLAGS $CMAKE_ARCH"
-  export LDLAGS="$LDLAGS $CMAKE_ARCH"
-fi
-
-cmake .. -G"$CMAKE_GENERATOR" \
--DBUILD_SHARED_LIBS=1 \
--DCMAKE_INSTALL_PREFIX=$PREFIX
-
-make
+mkdir build_glew
+cd build_glew
+cmake ../cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DBUILD_UTILS=1 -DGLEW_OSMESA=1
+make -j$CPU_COUNT
 make install
 
+cd ..
+mkdir build_test
+cd build_test
+cmake ../cmake/testbuild -DCMAKE_INSTALL_PREFIX=${PWD}/out -DCMAKE_PREFIX_PATH=${PWD}/out -DCMAKE_BUILD_TYPE=Release
+make -j$CPU_COUNT
+make install
+
+# Copy the libs just for testing
+cp $PREFIX/lib/libGLEW.dylib .
+out/bin/cmake-test
