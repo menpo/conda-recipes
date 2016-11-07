089b5743221a5b52a41300d2a00052c3b434c909
James Booth <jabooth@gmail.com>
2016-11-07 09:59:48 +0000

    fix include paths

--- scikit-sparse/build.sh
+++ scikit-sparse/build.sh
@@ -1,4 +1,6 @@
-export CFLAGS="-I$INCLUDE_PATH"
+export LIBRARY_PATH="$PREFIX/lib"
+export INCLUDE_PATH="$PREFIX/include"
+export CFLAGS="-I$INCLUDE_PATH/suitesparse"
 export LDFLAGS="-L$LIBRARY_PATH"
 
 "$PYTHON" setup.py install --single-version-externally-managed --record=/tmp/record.txt
